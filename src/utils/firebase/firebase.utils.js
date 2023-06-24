4e649bf1d3780f884a0df79c97a7688acf2e6325
captjhunter <hhjessica99@yahoo.com>
2023-06-24 15:27:05 -0400

    authentication and storage of users

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
@@ -23,3 +24,34 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', 'userAuth.uid');
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+//if user data does not exist
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try { 
+            await setDoc(userDocRef, {
+                displayName, email, createdAt
+            });
+        } catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+
+//if user data exists
+return userDocRef;
+
+
+  };
